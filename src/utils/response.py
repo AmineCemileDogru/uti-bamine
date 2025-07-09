cfcc522fdb00e5fe5cbd8d11e7ffbbd35771d67b
Deyto7 <115898497+Deyto7@users.noreply.github.com>
2025-07-09 16:14:12 +0300

    feats:Changed Package.py, response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,15 +1,15 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from components.BAmine.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, BAmineExecutorOutputs, BAmineExecutorResponse, BAmineExecutor, OutputImage
 
 
 def build_response(context):
     outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
-    packageConfigs = PackageConfigs(executor=executor)
+    bAmineExecutorOutputs = BAmineExecutorOutputs(outputImage=outputImage)
+    bAmineExecutorResponse = BAmineExecutorResponse(outputs=bAmineExecutorOutputs)
+    bAmineExecutor = BAmineExecutor(value=bAmineExecutorResponse)
+    configexecutor = ConfigExecutor(value=bAmineExecutor)
+    packageConfigs = PackageConfigs(executor=configexecutor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
     return packageModel
