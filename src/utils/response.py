5395e5b1f51e6e5fd40fc43cfcf32079cbf8d574
Deyto7 <115898497+Deyto7@users.noreply.github.com>
2025-07-14 17:19:50 +0300

    fix:Changed response.py,feat:Changed CAmine.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,25 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.BAmine.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, BAmineExecutorOutputs, BAmineExecutorResponse, BAmineExecutor, OutputImage
+from components.BAmine.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, BAmineExecutorOutputs, BAmineExecutorResponse, BAmineExecutor,CAmineExecutorOutputs, CAmineExecutorResponse, CAmineExecutor, OutputImage
 
 
-def build_response(context):
+def build_response_b(context):
     outputImage = OutputImage(value=context.image)
     bAmineExecutorOutputs = BAmineExecutorOutputs(outputImage=outputImage)
     bAmineExecutorResponse = BAmineExecutorResponse(outputs=bAmineExecutorOutputs)
     bAmineExecutor = BAmineExecutor(value=bAmineExecutorResponse)
     configexecutor = ConfigExecutor(value=bAmineExecutor)
+    packageConfigs = PackageConfigs(executor=configexecutor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+
+def build_response_c(context):
+    outputImage = OutputImage(value=context.image)
+    cAmineExecutorOutputs = CAmineExecutorOutputs(outputImage=outputImage)
+    cAmineExecutorResponse = CAmineExecutorResponse(outputs=cAmineExecutorOutputs)
+    cAmineExecutor = CAmineExecutor(value=cAmineExecutorResponse)
+    configexecutor = ConfigExecutor(value=cAmineExecutor)
     packageConfigs = PackageConfigs(executor=configexecutor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
