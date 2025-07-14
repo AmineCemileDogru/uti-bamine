e0643883f0c36e06ba63d2fa99b56de3335217b4
Deyto7 <115898497+Deyto7@users.noreply.github.com>
2025-07-14 23:25:20 +0300

    fix:Changed BAmine.py,CAmine.py

--- src/executors/BAmine.py
+++ src/executors/BAmine.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.BAmine.src.utils.response import build_response
+from components.BAmine.src.utils.response import build_response_b
 from components.BAmine.src.models.PackageModel import PackageModel
 
 
@@ -39,7 +39,7 @@ class BAmine(Component):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
         img.value = self.blurring(img.value)
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
-        packageModel = build_response(context=self)
+        packageModel = build_response_b(context=self)
         return packageModel
 
 if "__main__" == __name__:
