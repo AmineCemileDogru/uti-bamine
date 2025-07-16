857054d5c5aa719007f4d0a3fd1c794e3e2402eb
Deyto7 <115898497+Deyto7@users.noreply.github.com>
2025-07-16 11:27:43 +0300

    fix:Changed CAmine.py

--- src/executors/CAmine.py
+++ src/executors/CAmine.py
@@ -11,8 +11,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.CAmine.src.utils.response import build_response_c
-from components.CAmine.src.models.PackageModel import PackageModel
+from components.BAmine.src.utils.response import build_response_c
+from components.BAmine.src.models.PackageModel import PackageModel
 
 
 class CAmine(Component):
