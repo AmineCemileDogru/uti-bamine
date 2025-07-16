e5ca8c1d24990ec3be04919f7d6f52bf0bb57c73
Deyto7 <115898497+Deyto7@users.noreply.github.com>
2025-07-16 16:37:52 +0300

    fix:Changed BAmine.py

--- src/executors/BAmine.py
+++ src/executors/BAmine.py
@@ -36,7 +36,7 @@ class BAmine(Component):
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
         img.value = self.blurring(img.value)
-        self.imageOne = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
+        self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
         packageModel = build_response_b(context=self)
         return packageModel
 
